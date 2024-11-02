d7c92c2968a9fea22b7e0c9b75cbc7fc04a5a5d2
Alexander Y <startyourday60@gmail.com>
2024-11-02 08:54:05 +0000

    test: test logic for a test

--- number_guess.sh
+++ number_guess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo "Enter your username:"
+echo -e "Enter your username:"
 read username
 
 if [[ ! "$username" =~ ^[a-zA-Z0-9]{1,22}$ ]]; then
@@ -15,18 +15,19 @@ user_exists=$($PSQL "SELECT username FROM players WHERE username = '$username';"
 if [[ -n "$user_exists" ]]; then
     games_played=$($PSQL "SELECT playtimes FROM players WHERE username = '$username';")
     best_game=$($PSQL "SELECT bestscore FROM players WHERE username = '$username';")
-    echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
+    echo -e "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."
 else
-    echo "Welcome, $username! It looks like this is your first time here."
+    echo -e "Welcome, $username! It looks like this is your first time here."
     $PSQL "INSERT INTO players (username, playtimes, bestscore) VALUES ('$username', 0, 0);" 1> /dev/null 2> /dev/null
 fi
-
+games_played=$(( games_played + 1 ))
+$PSQL "UPDATE players SET playtimes=$games_played where username='$username'" 1> /dev/null
 NUM=$(( RANDOM % 1000 + 1 ))
 echo "Guess the secret number between 1 and 1000: "
 read guess
 if [[ $NUM -eq $guess ]];
 then
-	echo "You are guess"
+        echo "You are guess"
 else
-	echo "You don't guess"
+        echo "You don't guess"
 fi
